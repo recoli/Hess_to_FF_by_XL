2ac6b810ed66c1acb67f26b8ca867d18cc238054
Xin Li <lixin.reco@gmail.com>
2015-01-31 23:05:40 +0100

    updated readme and license info

--- Hess_to_FF_by_XL.pl
+++ Hess_to_FF_by_XL.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#***********************************************************************
+#*************************************************************************
 #                ==== HESSIAN >>> to >> FF ====
 #  Convert Hessian to bond stretching and angle bending parameters
 #  Hessian is read from Gaussian fchk file
@@ -8,19 +8,20 @@
 #
 #  Copyright (C) Xin Li <lixin.reco@gmail.com>
 #
-#  This program is free software: you can redistribute it and/or modify
+#  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
+#  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#***********************************************************************
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#*************************************************************************
 
 #  Usage: perl Hess_to_FF_by_XL.pl xxx.fchk [2-3 atoms]
 #  Ref: JM Seminario, Int. J. Quantum Chem., 1996, 60, 1271-1277.
@@ -48,8 +49,8 @@ $jobtype = "BOND";
 
 if (@ARGV==3)
 {
-	$ak = $ARGV[2] - 1;
-	$jobtype = "ANGLE";
+   $ak = $ARGV[2] - 1;
+   $jobtype = "ANGLE";
 }
 
 # read fchk file for coordinates and Hessian
@@ -60,44 +61,44 @@ my ($i, $j, $natoms, $ndf);
 open FL,"$fchk" or die;
 while(<FL>)
 {
-	# read coordinates
-	if (/Current cartesian coordinates/)
-	{
-		# number of atoms
-		$ndf = (split)[5];
-		$natoms = $ndf/3;
+   # read coordinates
+   if (/Current cartesian coordinates/)
+   {
+      # number of atoms
+      $ndf = (split)[5];
+      $natoms = $ndf/3;
 
       # 3N coordinates
-		$i = 0;
-		while(<FL>)
-		{
-			@_ = (split);
-			for $j ( 0 .. @_-1 )
-			{
-				$g09coord[$i] = $_[$j];
-				$i++;
-			}
-			last if $i==$ndf;
-		}
+      $i = 0;
+      while(<FL>)
+      {
+         @_ = (split);
+         for $j ( 0 .. @_-1 )
+         {
+            $g09coord[$i] = $_[$j];
+            $i++;
+         }
+         last if $i==$ndf;
+      }
       die "Error in reading coordiantes!\n" unless $i==$ndf;
-	}
+   }
 
-	# read hessian, 3N*(3N+1)/2 elements
-	if (/Cartesian Force Constants/)
-	{
+   # read hessian, 3N*(3N+1)/2 elements
+   if (/Cartesian Force Constants/)
+   {
       $i = 0;
       while (<FL>)
       {
          @_ = (split);
          for $j ( 0 .. @_-1 )
          {
-         	$g09hess[$i] = $_[$j];
-         	$i++;
+            $g09hess[$i] = $_[$j];
+            $i++;
          }
          last if $i==$ndf*($ndf+1)/2;
       }
       die "Error in reading Hessian!\n" unless $i==$ndf*($ndf+1)/2;
-	}
+   }
 }
 close FL;
 
@@ -105,9 +106,9 @@ close FL;
 
 for $i ( 0 .. $natoms-1 )
 {
-	$coord[$i][0] = $g09coord[$i*3];
-	$coord[$i][1] = $g09coord[$i*3+1];
-	$coord[$i][2] = $g09coord[$i*3+2];
+   $coord[$i][0] = $g09coord[$i*3];
+   $coord[$i][1] = $g09coord[$i*3+1];
+   $coord[$i][2] = $g09coord[$i*3+2];
 }
 
 # compute vectors
@@ -130,14 +131,14 @@ $r0 *= (0.52917721092 * 0.1); # nm
 
 if ($jobtype eq "ANGLE")
 {
-	# k->j
+   # k->j
    $vec_kj[0] = $coord[$aj][0] - $coord[$ak][0];
    $vec_kj[1] = $coord[$aj][1] - $coord[$ak][1];
    $vec_kj[2] = $coord[$aj][2] - $coord[$ak][2];
    $r_kj = dist(@vec_kj);
    @vec_kj = norm(@vec_kj);
 
-	$a0 = acos(inn_p(@vec_ij, @vec_kj))/pi * 180.0;
+   $a0 = acos(inn_p(@vec_ij, @vec_kj))/pi * 180.0;
    
    my @vec_N = norm(out_p(@vec_kj, @vec_ij));
    @vec_PA = out_p(@vec_N, @vec_ij);
@@ -149,12 +150,12 @@ if ($jobtype eq "ANGLE")
 my $count = 0;
 for $i ( 0 .. $ndf )
 {
-	for $j ( 0 .. $i )
-	{
-		$hess[$i][$j] = $g09hess[$count];
-		$hess[$j][$i] = $hess[$i][$j] if $i!=$j;
-		$count++;
-	}
+   for $j ( 0 .. $i )
+   {
+      $hess[$i][$j] = $g09hess[$count];
+      $hess[$j][$i] = $hess[$i][$j] if $i!=$j;
+      $count++;
+   }
 }
 
 # Calculate hessian matrix for pair potential
@@ -163,26 +164,26 @@ my (@AB, @CB);
 
 for $i ( 0 .. 2 )
 {
-	for $j ( 0 .. 2 )
-	{
-		$AB[$i*3+$j] = -$hess[$ai*3+$i][$aj*3+$j];
-		$CB[$i*3+$j] = -$hess[$ak*3+$i][$aj*3+$j] if ($jobtype eq "ANGLE");
-	}
+   for $j ( 0 .. 2 )
+   {
+      $AB[$i*3+$j] = -$hess[$ai*3+$i][$aj*3+$j];
+      $CB[$i*3+$j] = -$hess[$ak*3+$i][$aj*3+$j] if ($jobtype eq "ANGLE");
+   }
 }
 
 # calculate force constant
 
 if ($jobtype eq "BOND")
 {
-	my $kbond = CalcBond(@AB, @vec_ij);
+   my $kbond = CalcBond(@AB, @vec_ij);
    printf "%-8d%-8d%-8d%15.6e%15.6e\n", 
-			 $ai+1, $aj+1, 1, $r0, $kbond;
+          $ai+1, $aj+1, 1, $r0, $kbond;
 }
 elsif ($jobtype eq "ANGLE")
 {
-	my $kangle = CalcAngle(@AB, @CB, @vec_PA, @vec_PC);
+   my $kangle = CalcAngle(@AB, @CB, @vec_PA, @vec_PC);
    printf "%-7d%-7d%-7d%-7d%15.6e%15.6e\n", 
-			 $ai+1, $aj+1, $ak+1, 1, $a0, $kangle;
+          $ai+1, $aj+1, $ak+1, 1, $a0, $kangle;
 }
 
 ### subroutines
@@ -193,7 +194,7 @@ sub GetEigValue
 {
    unless (@_-9==0)
    {
-   	die "Error: Only 3x3 matrix is supported in GetEigValue!\n";
+      die "Error: Only 3x3 matrix is supported in GetEigValue!\n";
    }
 
    # save 9 elements in @A
@@ -201,7 +202,7 @@ sub GetEigValue
    my ($i,@A);
    for $i ( 0 .. 8 )
    {
-   	$A[$i] = $_[$i];
+      $A[$i] = $_[$i];
    }
 
    # compute the eigenvalues
@@ -211,8 +212,8 @@ sub GetEigValue
    my @AqI;
    for $i ( 0 .. 8 )
    {
-   	$AqI[$i] = $A[$i];
-   	$AqI[$i] -= $q if ($i==0 or $i==4 or $i==8);
+      $AqI[$i] = $A[$i];
+      $AqI[$i] -= $q if ($i==0 or $i==4 or $i==8);
    }
    my $p = m_tr(m_mult(@AqI,@AqI));
    $p = sqrt($p/6.0);
@@ -220,21 +221,21 @@ sub GetEigValue
    my @B;
    for $i ( 0 .. 8 )
    {
-   	my $tmp = 0.0;
-   	$tmp = 1.0 if ($i==0 or $i==4 or $i==8);
-   	$B[$i] = 1.0/$p * ($A[$i]-$q*$tmp);
+      my $tmp = 0.0;
+      $tmp = 1.0 if ($i==0 or $i==4 or $i==8);
+      $B[$i] = 1.0/$p * ($A[$i]-$q*$tmp);
    }
 
    my $r = calcDET(@B)/2.0;
    if ($r<=-1)
    {
-   	#printf "Warning: \$r=$r is smaller than -1.0!\n";
-   	$r = -1;
+      #printf "Warning: \$r=$r is smaller than -1.0!\n";
+      $r = -1;
    }
    elsif ($r>=1)
    {
-   	#printf "Warning: \$r=$r is greater than 1.0!\n";
-   	$r = 1;
+      #printf "Warning: \$r=$r is greater than 1.0!\n";
+      $r = 1;
    }
    my $phi = acos($r)/3;
 
@@ -286,7 +287,7 @@ sub GetEigVector
    my $y = calcDET(@b2)/calcDET(@a);
    my $z = 1.0;
    
-	# check the result
+   # check the result
    my $check = $tmp[6]*$x + $tmp[7]*$y + $tmp[8]*$z;
    if (abs($check)>1.0e-3)
    {
@@ -306,31 +307,31 @@ sub GetEigVector
 
 sub calcDET
 {
-	my $r;
-	if (@_-4==0)
-	{
-		$r = $_[0]*$_[3] - $_[1]*$_[2];
-	}
-	elsif (@_-9==0)
-	{
-		$r = $_[0]*$_[4]*$_[8] + $_[1]*$_[5]*$_[6] + $_[2]*$_[3]*$_[7]
-			- $_[2]*$_[4]*$_[6] - $_[1]*$_[3]*$_[8] - $_[0]*$_[5]*$_[7];
-	}
-	else
-	{
-		die "Error: Only 2x2 and 3x3 matrices are supported in subroutine calcDET!\n";
-	}
-	return $r;
+   my $r;
+   if (@_-4==0)
+   {
+      $r = $_[0]*$_[3] - $_[1]*$_[2];
+   }
+   elsif (@_-9==0)
+   {
+      $r = $_[0]*$_[4]*$_[8] + $_[1]*$_[5]*$_[6] + $_[2]*$_[3]*$_[7]
+         - $_[2]*$_[4]*$_[6] - $_[1]*$_[3]*$_[8] - $_[0]*$_[5]*$_[7];
+   }
+   else
+   {
+      die "Error: Only 2x2 and 3x3 matrices are supported in subroutine calcDET!\n";
+   }
+   return $r;
 }
 
 # calculate the trace of a 3x3 matrix
 
 sub m_tr
 {
-	die "Incorrect number of elements in subroutine m_tr!\n" 
-		unless @_==9;
-	my $c = $_[0]+$_[4]+$_[8];
-	return ($c);
+   die "Incorrect number of elements in subroutine m_tr!\n" 
+      unless @_==9;
+   my $c = $_[0]+$_[4]+$_[8];
+   return ($c);
 }
 
 # multiplication of 3x3 matrices
@@ -397,7 +398,7 @@ sub out_p
 sub norm
 {
    my @a;
-	my $r = sqrt($_[0]**2+$_[1]**2+$_[2]**2);
+   my $r = sqrt($_[0]**2+$_[1]**2+$_[2]**2);
    for $i ( 0 .. 2 )
    {
       $a[$i] = $_[$i]/$r;
@@ -409,17 +410,17 @@ sub norm
 
 sub dist
 {
-	my $r = sqrt($_[0]**2+$_[1]**2+$_[2]**2);
-	return ($r);
+   my $r = sqrt($_[0]**2+$_[1]**2+$_[2]**2);
+   return ($r);
 }
 
 # calculate bond stretching parameters
 
 sub CalcBond
 {
-	my ($i,@AB,@vec_ij);
-	for $i ( 0 .. 8 )	{	$AB[$i] = shift(@_);	}
-	for $i ( 0 .. 2 )	{	$vec_ij[$i] = shift(@_);	}
+   my ($i,@AB,@vec_ij);
+   for $i ( 0 .. 8 )   {   $AB[$i] = shift(@_);   }
+   for $i ( 0 .. 2 )   {   $vec_ij[$i] = shift(@_);   }
 
    # compute the eigenvalues of @AB
     
@@ -435,20 +436,20 @@ sub CalcBond
    
    $k *= 6.02214129 * 4.35974434 * 100.0; # kJ/mol Bohr^-2
    $k /= (0.52917721092 * 0.1) ** 2;      # kJ/mol nm^-2
-	return ($k);
+   return ($k);
 }
 
 # calculate angle bending parameters
 
 sub CalcAngle
 {
-	my ($i, @AB, @CB, @vec_PA, @vec_PC);
-	for $i (0 .. 8) { $AB[$i] = shift(@_); }
-	for $i (0 .. 8) { $CB[$i] = shift(@_); }
-	for $i (0 .. 2) { $vec_PA[$i] = shift(@_); }
-	for $i (0 .. 2) { $vec_PC[$i] = shift(@_); }
+   my ($i, @AB, @CB, @vec_PA, @vec_PC);
+   for $i (0 .. 8) { $AB[$i] = shift(@_); }
+   for $i (0 .. 8) { $CB[$i] = shift(@_); }
+   for $i (0 .. 2) { $vec_PA[$i] = shift(@_); }
+   for $i (0 .. 2) { $vec_PC[$i] = shift(@_); }
 
-	@_ = GetEigValue(@AB);
+   @_ = GetEigValue(@AB);
    my $eig0 = shift(@_);
    my $eig1 = shift(@_);
    my $eig2 = shift(@_);
@@ -457,7 +458,7 @@ sub CalcAngle
           + $eig1 * abs($vec_PA[0]*$eigvec[3] + $vec_PA[1]*$eigvec[4] + $vec_PA[2]*$eigvec[5])
           + $eig2 * abs($vec_PA[0]*$eigvec[6] + $vec_PA[1]*$eigvec[7] + $vec_PA[2]*$eigvec[8]);
 
-	@_ = GetEigValue(@CB);
+   @_ = GetEigValue(@CB);
    $eig0 = shift(@_);
    $eig1 = shift(@_);
    $eig2 = shift(@_);
@@ -468,6 +469,6 @@ sub CalcAngle
    
    my $kangle = 1.0 / (1.0 / ($r_ij**2 * $k1) + 1.0 / ($r_kj**2 * $k2));
    $kangle *= 6.02214129 * 4.35974434 * 100.0;      # kJ/mol
-	return ($kangle);
+   return ($kangle);
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hess_to_FF_by_XL
 Convert Hessian to bond stretching and angle bending parameters
+Hessian is read from Gaussian fchk file
+Output parameters are in GROMACS format
